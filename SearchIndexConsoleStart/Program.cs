151c90d4264015a8274c1c2d26b305a385ead014
Richard Chalk <barmybrit@gmail.com>
2023-03-13 07:03:29 +0100

    Added all queries

--- SearchIndexConsoleStart/Program.cs
+++ SearchIndexConsoleStart/Program.cs
@@ -4,6 +4,7 @@ using Azure.Search.Documents.Indexes;
 using Azure.Search.Documents.Indexes.Models;
 using Azure.Search.Documents.Models;
 using SearchIndexConsoleEnd.Data;
+using System.Reflection.Metadata;
 
 // Den här appen skapar ett Hotels-index som du läser in med hotelldata och kör frågor mot.
 // https://learn.microsoft.com/sv-se/azure/search/search-get-started-dotnet
@@ -206,8 +207,9 @@ class Program
         SearchOptions options;
         SearchResults<Hotel> response;
 
-        // Query 1
-        Console.WriteLine("Query #1: Search on empty term '*' to return all documents, showing a subset of fields...\n");
+        // Query 1 ///////////////////////////////////////////////////////////////////
+        Console.WriteLine("Query #1: Search on empty term '*' to return all documents, " +
+            "showing a subset of fields...\n");
 
         options = new SearchOptions()
         {
@@ -223,6 +225,82 @@ class Program
         response = srchclient.Search<Hotel>("*", options);
         WriteDocuments(response);
 
+        // Query 2 ///////////////////////////////////////////////////////////////////
+        // söker du efter en term, lägger till ett filter som väljer dokument där
+        // Klassificering är större än 4 och sorterar sedan efter Klassificering i
+        // fallande ordning. Filter är ett booleskt uttryck som utvärderas över
+        // IsFilterable-fält i ett index. Filterfrågor inkluderar eller exkluderar värden. 
+        Console.WriteLine("Query #2: Search on 'hotels', filter on 'Rating gt 4', " +
+            "sort by Rating in descending order...\n");
+
+        options = new SearchOptions()
+        {
+            Filter = "Rating gt 4",
+            OrderBy = { "Rating desc" }
+        };
+
+        options.Select.Add("HotelId");
+        options.Select.Add("HotelName");
+        options.Select.Add("Rating");
+
+        response = srchclient.Search<Hotel>("hotels", options);
+        WriteDocuments(response);
+
+        // Query 3 ////////////////////////////////////////////////////////////////////
+        //  visar searchFields, som används för att begränsa en fulltextsökningsåtgärd
+        //  till specifika fält.
+        Console.WriteLine("Query #3: Limit search to specific fields " +
+            "(pool in Tags field)...\n");
+
+        options = new SearchOptions()
+        {
+            SearchFields = { "Tags" }
+        };
+
+        options.Select.Add("HotelId");
+        options.Select.Add("HotelName");
+        options.Select.Add("Tags");
+
+        response = srchclient.Search<Hotel>("pool", options);
+        WriteDocuments(response);
+
+        // Query 4 /////////////////////////////////////////////////////////////////////
+        // visar faser, som kan användas för att strukturera en aspektbaserad
+        // navigeringsstruktur.
+        Console.WriteLine("Query #4: Facet on 'Category'...\n");
+
+        options = new SearchOptions()
+        {
+            Filter = ""
+        };
+
+        options.Facets.Add("Category");
+
+        options.Select.Add("HotelId");
+        options.Select.Add("HotelName");
+        options.Select.Add("Category");
+
+        response = srchclient.Search<Hotel>("*", options);
+        WriteDocuments(response);
+
+        // Query 5 ///////////////////////////////////////////////////////////////////////
+        // Returnerar du ett specifikt dokument. En dokumentsökning är ett typiskt svar
+        // på OnClick-händelsen i en resultatuppsättning.
+        Console.WriteLine("Query #5: Look up a specific document...\n");
+
+        Response<Hotel> lookupResponse;
+        lookupResponse = srchclient.GetDocument<Hotel>("3");
+
+        Console.WriteLine(lookupResponse.Value.HotelId);
+
+        // Query 6 ///////////////////////////////////////////////////////////////////////
+        // Visar syntaxen för automatisk komplettering, vilket simulerar en partiell
+        // användarinmatning av "sa" som matchar två möjliga matchningar i de sourceFields som är associerade med den förslagsanvändare som du definierade i indexet.
+        Console.WriteLine("Query #6: Call Autocomplete on HotelName that starts with " +
+            "'sa'...\n");
+
+        var autoresponse = srchclient.Autocomplete("sa", "sg");
+        WriteDocuments(autoresponse);
     }
 
     // Write search results to console //////////////////////////////////////////////////////////
